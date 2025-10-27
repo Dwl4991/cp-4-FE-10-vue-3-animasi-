68788a15c254f778c5982cac616be6d5d69d68dc
Dwl4991 <candra.dwiwaluyo@gmail.com>
2025-10-28 05:02:14 +0800

    video 5 done

--- todo-animation/src/views/HomeView.vue
+++ todo-animation/src/views/HomeView.vue
@@ -1,14 +1,26 @@
 <script setup>
 import { ref } from "vue";
+
+const tasks = ref(["Learn Vue 3", "Learn CSS", "Learn JavaScript"]);
+const newTask = ref("");
+
+function addTask() {
+  if (newTask.value) {
+    tasks.value.unshift(newTask.value);
+    newTask.value = "";
+  }
+}
+
+function removeTask(task) {
+  tasks.value = tasks.value.filter((t) => t !== task);
+}
 </script>
 
 <template>
   <main>
     <div class="container">
-      <input type="text" autofocus />
-      <div>Task 1</div>
-      <div>Task 2</div>
-      <div>Task 3</div>
+      <input type="text" autofocus v-model="newTask" @keyup.enter="addTask()" />
+      <div class="card-list" v-for="task in tasks" :key="task" @click="removeTask(task)">{{ task }}</div>
     </div>
   </main>
 </template>
@@ -25,5 +37,16 @@ import { ref } from "vue";
   border: 1px solid #ccc;
   margin-bottom: 20px;
   box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+}
+
+.card-list {
+  width: 300px;
+  background-color: #f9f9f9;
+  padding: 5px 10px;
+  margin-top: 10px;
+  border-radius: 5px;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  text-align: center;
+  cursor: pointer;
 }
 </style>
